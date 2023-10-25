9478c7b682d08f4f4d10b34800f9b0c256743d72
Pawlo77 <pawpoz16@gmail.com>
2023-10-26 00:27:04 +0200

    intro

---
+++ intro/bad_formatting.py
@@ -0,0 +1,16 @@
+# was
+"""
+x=5
+y=10
+if x>y:
+            print('x is greater than y')
+else:
+    print('x is not greater than y')
+"""
+
+x = 5
+y = 10
+if x > y:
+    print("x is greater than y")
+else:
+    print("x is not greater than y")
